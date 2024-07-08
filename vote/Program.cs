a0d366b00964ecbb38ca6dd72b9e8b5ef08f518c
elenagyy <elenag.yankova2@gmail.com>
2024-07-08 14:59:00 +0300

    my first project

---
+++ vote/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using vote;
+Console.OutputEncoding = Encoding.UTF8;
+
+Voting voting = new Voting();
+Console.WriteLine("Изберете за кого да гласувате (с 1 за Кандидат 1 и с 2 за Кандидат 2; натиснете 0 за край).");
+Console.WriteLine();
+while (true)
+{
+    Console.Write("Вашият избор е: ");
+    int choice = int.Parse(Console.ReadLine());
+
+    if (choice == 0)
+    {
+        break;
+    }
+
+    voting.Vote(choice);
+}
+Console.WriteLine();
+voting.DisplayResults();
---
+++ vote/Voting.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vote
+{
+    internal class Voting
+    {
+        private int choice;
+        private int candidate1;
+        private int candidate2;
+        private int totalVotes;
+
+       
+        public Voting()
+        {
+            candidate1 = 0;
+            candidate2 = 0;
+            totalVotes = 0;
+        }
+
+        public void Vote(int choice)
+        {
+            if (choice == 1)
+            {
+                candidate1++;
+            }
+            else if (choice == 2) 
+            {
+                candidate2++;
+            }
+            else
+            {
+                Console.WriteLine("Невалиден избор.");
+                return;
+            }
+
+            totalVotes++;
+        }
+
+        public void DisplayResults()
+        {
+            Console.WriteLine($"Гласували общо: {totalVotes}");
+            Console.WriteLine($"Кандидат 1: {candidate1}");
+            Console.WriteLine($"Кандидат 2: {candidate2}");
+            if (candidate1 > candidate2)
+            {
+                Console.WriteLine("Кандидат 1 печели изборите!");
+            }
+            else if (candidate1 < candidate2)
+            {
+                Console.WriteLine("Кандидат 2 печели изборите!");
+            }
+            else
+            {
+                Console.WriteLine("Ще има балотаж!");
+            }
+        }
+    }
+
+    
+}
